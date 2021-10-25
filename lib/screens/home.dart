0425cddbe641ce23d4f2ab33aac4944f1044944c
Tobias Neidhardt <tobias@Mini-von-Tobias.fritz.box>
2021-10-25 14:17:57 +0200

    create home screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,3 +1,7 @@
+import 'package:feed_me/constants/colors.dart';
+import 'package:feed_me/constants/cook_book.dart';
+import 'package:feed_me/constants/feed_me_circle_avatar.dart';
+import 'package:feed_me/constants/search_text_form_field.dart';
 import 'package:feed_me/registration_and_login/auth_service.dart';
 import 'package:flutter/material.dart';
 
@@ -13,11 +17,43 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return TextButton(
-      onPressed: () async {
-        await _auth.signOut();
-      },
-      child: const Text("logout"),
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: Colors.white,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: BasicGreen,
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+      floatingActionButtonLocation:FloatingActionButtonLocation.centerFloat,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            color: BasicGreen,
+            height: size.height * 0.4,
+            width: size.width,
+            child: Column(
+              children: [FeedMeCircleAvatar(radius: 120), SearchTextFormField(hintText: "Nach Rezepten suchen",)],
+            ),
+          ),
+          SizedBox(height: size.height * 0.02),
+          Row(
+            children: [
+              CookBook(text: "Leckeres Essen", onPress: () {print("test");}),
+              CookBook(text: "Leckeres Essen2", onPress: () {}),
+            ],
+          )
+        ],
+      ),
     );
+
+    //
+    //   TextButton(
+    //   onPressed: () async {
+    //     await _auth.signOut();
+    //   },
+    //   child: const Text("logout"),
+    // );
   }
 }
