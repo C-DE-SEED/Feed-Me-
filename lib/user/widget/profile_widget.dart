0f6e20eca4e653a302122e6b7f6e83c069d26527
Drescher, Maximilian <maximilian.drescher@student.fhws.de>
2021-10-25 15:50:50 +0200

    wip

--- lib/user/widget/profile_widget.dart
+++ lib/user/widget/profile_widget.dart
@@ -1,6 +1,7 @@
 import 'package:feed_me/constants/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
 class ProfileWidget extends StatelessWidget {
   final String imagePath;
@@ -11,7 +12,9 @@ class ProfileWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
+    Size size = MediaQuery
+        .of(context)
+        .size;
     PickedFile imageFile;
 
     final picker = ImagePicker();
@@ -30,60 +33,42 @@ class ProfileWidget extends StatelessWidget {
     );
   }
 
-  Widget buildImage(Size size, String imagePath){
-    if(imagePath.isEmpty){
-
+  Future<Widget> buildImage(Size size, String imagePath) async {
+     Image image = (await ImagePicker().getImage(
+        source: ImageSource.gallery,
+      )) as Image;
     }
-    final image = NetworkImage(imagePath);
-    return ClipOval(
-      child: Material(
-        color: Colors.transparent,
-        child: Ink.image(
-          image: image,
-          fit: BoxFit.cover,
-          width: size.height * 0.18,
-          height: size.height * 0.18,
-          child: InkWell(onTap: onClicked),
+    return CircleAvatar(
+        backgroundImage: image.image,
+        radius: size.height * 0.18,
+        backgroundColor: Colors.transparent);
+  }
+}
+
+Widget buildEditIcon(Color color) =>
+    buildCircle(
+      color: BasicGreen,
+      all: 2,
+      child: buildCircle(
+        color: color,
+        all: 6,
+        child: const Icon(
+          Icons.add_a_photo_outlined,
+          color: BasicGreen,
+          size: 25,
         ),
       ),
     );
-  }
 
-  Widget buildEditIcon(Color color) => buildCircle(
-        color: BasicGreen,
-        all: 2,
-        child: buildCircle(
-          color: color,
-          all: 6,
-          child: const Icon(
-            Icons.add_a_photo_outlined,
-            color: BasicGreen,
-            size: 25,
-          ),
-        ),
-      );
-
-  Widget buildCircle({
-    @required Widget child,
-    @required double all,
-    @required Color color,
-  }) =>
-      ClipOval(
-        child: Container(
-          padding: EdgeInsets.all(all),
-          color: color,
-          child: child,
-        ),
-      );
-
-  void _openGallery(BuildContext context) async{
-    final pickedFile = await ImagePicker().getImage(
-      source: ImageSource.gallery ,
-    );
-    setState(() {
-      imageFile = pickedFile!;
-    );
-
-    Navigator.pop(context);
-  }
-}
+Widget buildCircle({
+  @required Widget child,
+  @required double all,
+  @required Color color,
+}) =>
+    ClipOval(
+      child: Container(
+        padding: EdgeInsets.all(all),
+        color: color,
+        child: child,
+      ),
+    );}
