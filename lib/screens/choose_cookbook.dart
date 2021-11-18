62070681a1e58fc6c1a4329db6ead695f6fd4c50
Drescher, Maximilian <maximilian.drescher@student.fhws.de>
2021-11-18 20:07:20 +0100

    insert app icon

--- lib/screens/choose_cookbook.dart
+++ lib/screens/choose_cookbook.dart
@@ -40,115 +40,106 @@ class _ChooseCookbookState extends State<ChooseCookbook> {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: basicColor,
-      body: SafeArea(
-        child: ListView(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.fromLTRB(16.0,0.0,16.0,15.0),
-              child: Container(
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    border: Border.all(color: Colors.white),
-                    borderRadius: const BorderRadius.vertical(
-                        top: Radius.circular(20.0),
-                        bottom: Radius.circular(0.0))),
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Padding(
-                          padding: const EdgeInsets.all(15.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Text(
-                                  "Hallo, " + authService.getUser().displayName,
-                                  style: const TextStyle(
-                                      fontSize: 18.0,
-                                      fontWeight: FontWeight.bold,
-                                      fontFamily: openSansFontFamily)),
-                              const SizedBox(
-                                height: 5.0,
-                              ),
-                              Text(
-                                "Was möchtest du heute kochen?",
-                                style: TextStyle(
-                                    color: Colors.grey.shade700,
-                                    fontFamily: openSansFontFamily),
-                              )
-                            ],
+      body: ListView(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16.0,0.0,16.0,15.0),
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: Colors.white),
+                  borderRadius: const BorderRadius.vertical(
+                      top: Radius.circular(20.0),
+                      bottom: Radius.circular(0.0))),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.all(15.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                                "Hallo, " + authService.getUser().displayName,
+                                style: const TextStyle(
+                                    fontSize: 18.0,
+                                    fontWeight: FontWeight.bold,
+                                    fontFamily: openSansFontFamily)),
+                            const SizedBox(
+                              height: 5.0,
+                            ),
+                            Text(
+                              "Was möchtest du heute kochen?",
+                              style: TextStyle(
+                                  color: Colors.grey.shade700,
+                                  fontFamily: openSansFontFamily),
+                            )
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0.0,10.0,10.0,0.0),
+                        child: CircleAvatar(
+                          backgroundImage:
+                          CachedNetworkImageProvider(
+                            authService.getUser().photoURL,
+                          ),
+                          radius: size.width * 0.09,
+                          child: TextButton(
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          const ProfilePage()));
+                            },
+                            child: null,
                           ),
                         ),
-                        Padding(
-                          padding: const EdgeInsets.fromLTRB(0.0,10.0,10.0,0.0),
-                          child: CircleAvatar(
-                            backgroundImage:
-                            CachedNetworkImageProvider(
-                              authService.getUser().photoURL,
-                            ),
-                            radius: size.width * 0.09,
-                            child: TextButton(
-                              onPressed: () {
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) =>
-                                            const ProfilePage()));
-                              },
-                              child: null,
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
-                    Material(
-                        elevation: 5.0,
-                        child: TextField(
-                            onChanged: (value) {
-                              //TODO insert filtered value
-                            },
-                            showCursor: true,
-                            decoration: const InputDecoration(
-                              hintText: "Nach Rezept suchen",
-                              prefixIcon:
-                                  Icon(Icons.search, color: Colors.black54),
-                              border: InputBorder.none,
-                            ))),
-                  ],
-                ),
+                      )
+                    ],
+                  ),
+                  Material(
+                      elevation: 5.0,
+                      child: TextField(
+                          onChanged: (value) {
+                            //TODO insert filtered value
+                          },
+                          showCursor: true,
+                          decoration: const InputDecoration(
+                            hintText: "Nach Rezept suchen",
+                            prefixIcon:
+                                Icon(Icons.search, color: Colors.black54),
+                            border: InputBorder.none,
+                          ))),
+                ],
               ),
             ),
-            GestureDetector(
-                onTap: () => _openDestinationPage(context),
-                child: _buildFeaturedItem(
-                    image:
-                        "https://firebasestorage.googleapis.com/v0/b/feed-me-b8533.appspot.com/o/recipe_images%2FRed%20Curry%2F1.png?alt=media&token=bcfdf574-b959-45ff-a251-a171b2969161",
-                    title: "Plant Food Factory's Kochbuch",
-                    subtitle: "Gesund & Lecker")),
-            GestureDetector(
-                onTap: () => _openDestinationPage(context),
-                child: _buildFeaturedItem(
-                    image:
-                        "https://www.takt-magazin.de/wp-content/uploads/2021/05/edgar-castrejon-1SPu0KT-Ejg-unsplash-scaled-e1622116685668-1170x855.jpg?x48126",
-                    title: "Laura's Rezepte",
-                    subtitle: "Frisch & Lecker")),
-            GestureDetector(
-                onTap: () => _openDestinationPage(context),
-                child: _buildFeaturedItem(
-                    image:
-                        "https://i.pinimg.com/originals/cd/88/e9/cd88e9b8c1875b7813d6af93343040d8.jpg",
-                    title: "Kim's Ideen",
-                    subtitle: "Ausgefallen & Frech")),
-            // GestureDetector(
-            //     onTap: () => _openDestinationPage(context),
-            //     child: _buildFeaturedItem(
-            //         image:
-            //         "https://www.belderbusch.de/wp-content/uploads/2018/09/vegetarisch-vegan-restaurant-essen-kln-bonn-5-von-5.jpg",
-            //         title: "Pokhara",
-            //         subtitle: "40 places worth to visit")),
-          ],
-        ),
+          ),
+          GestureDetector(
+              onTap: () => _openDestinationPage(context),
+              child: _buildFeaturedItem(
+                  image:
+                      "https://firebasestorage.googleapis.com/v0/b/feed-me-b8533.appspot.com/o/recipe_images%2FRed%20Curry%2F1.png?alt=media&token=bcfdf574-b959-45ff-a251-a171b2969161",
+                  title: "Plant Food Factory's Kochbuch",
+                  subtitle: "Gesund & Lecker")),
+          GestureDetector(
+              onTap: () => _openDestinationPage(context),
+              child: _buildFeaturedItem(
+                  image:
+                      "https://www.takt-magazin.de/wp-content/uploads/2021/05/edgar-castrejon-1SPu0KT-Ejg-unsplash-scaled-e1622116685668-1170x855.jpg?x48126",
+                  title: "Laura's Rezepte",
+                  subtitle: "Frisch & Lecker")),
+          GestureDetector(
+              onTap: () => _openDestinationPage(context),
+              child: _buildFeaturedItem(
+                  image:
+                      "https://i.pinimg.com/originals/cd/88/e9/cd88e9b8c1875b7813d6af93343040d8.jpg",
+                  title: "Kim's Ideen",
+                  subtitle: "Ausgefallen & Frech")),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         tooltip: ' Kochbuch\nhinzufügen',
