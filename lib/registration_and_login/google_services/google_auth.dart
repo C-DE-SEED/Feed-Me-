02347b0bcd3c76659b79446c9cbbe503d0117f20
Drescher, Maximilian <maximilian.drescher@student.fhws.de>
2021-11-23 14:21:36 +0100

    insert google login

---
+++ lib/registration_and_login/google_services/google_auth.dart
@@ -0,0 +1,61 @@
+import 'package:feed_me/constants/snack_bar.dart';
+import 'package:flutter/material.dart';
+import '../auth_service.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import '../google_sign_in.dart';
+
+
+class Authentication {
+  static Future<User> signInWithGoogle({ BuildContext context})
+  async {
+    final AuthService auth = AuthService();
+
+     var user = auth.getUser();
+
+    const GoogleSignIn googleSignIn = GoogleSignIn();
+
+    final GoogleSignInAccount? googleSignInAccount =
+    await googleSignIn.signIn();
+
+    if (googleSignInAccount != null) {
+      final GoogleSignInAuthentication googleSignInAuthentication =
+      await googleSignInAccount.authentication;
+
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleSignInAuthentication.accessToken,
+        idToken: googleSignInAuthentication.idToken,
+      );
+
+      try {
+        final UserCredential userCredential =
+        await auth.signInWithCredential(credential);
+
+        user = userCredential.user;
+      } on FirebaseAuthException catch (e) {
+        if (e.code == 'account-exists-with-different-credential') {
+          ScaffoldMessenger.of(context).showSnackBar(
+            customSnackBar(
+              content:
+              'The account already exists with a different credential.',
+            ),
+          );
+        } else if (e.code == 'invalid-credential') {
+          ScaffoldMessenger.of(context).showSnackBar(
+            customSnackBar(
+              content:
+              'Error occurred while accessing credentials. Try again.',
+            ),
+          );
+        }
+      } catch (e) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          customSnackBar(
+            content: 'Error occurred using Google Sign-In. Try again.',
+          ),
+        );
+      }
+
+    return user;
+  }
+
+}
