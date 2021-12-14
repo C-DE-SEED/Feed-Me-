114d5308b576bf2b3f1587089efe78f7fa0606fc
Drescher, Maximilian <maximilian.drescher@student.fhws.de>
2021-12-14 12:56:08 +0100

    insert favorite list view

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -11,6 +11,7 @@ import 'package:flutter/material.dart';
 import '../model/recipe_db_object.dart';
 import '../model/recipe_object.dart';
 import 'create_new_cook_book_1.dart';
+import 'open_cookbook/detail_page.dart';
 
 class Home extends StatefulWidget {
   const Home({
@@ -29,12 +30,27 @@ class _HomeState extends State<Home> {
   int cookbookCount = 0;
   List<Recipe> plantFoodFactory = [];
   List<Cookbook> userCookbooks = [];
+  List<Recipe> favoriteRecipes = [];
   var refreshKey = GlobalKey<RefreshIndicatorState>();
 
-  void getAllRecipes() async {
+  void getAllPlantFoodFactoryRecipes() async {
     plantFoodFactory = await RecipeDbObject()
         .getRecipesFromPlantFoodFactory("plant_food_factory")
         .elementAt(0);
+
+    addFavoriteRecipes();
+  }
+
+  List<String> filterSteps(Recipe recipe) {
+    List<String> x = [];
+    x = recipe.description.split("/");
+    return x;
+  }
+
+  List<String> filterIngredients(Recipe recipe) {
+    List<String> x = [];
+    x = recipe.ingredientsAndAmount.split("/");
+    return x;
   }
 
   Future<void> getCookBooks() async {
@@ -54,10 +70,23 @@ class _HomeState extends State<Home> {
     return await await recipeDbObject.getAllCookBooksFromUser();
   }
 
+  void addFavoriteRecipes() {
+    userCookbooks.forEach((element) {
+      element.recipes.forEach((element) {
+        //TODO getIsFavorite Element from db and add element.isFavorite
+        favoriteRecipes.add(element);
+      });
+    });
+    plantFoodFactory.forEach((element) {
+      //TODO getIsFavorite Element from db and add element.isFavorite
+      favoriteRecipes.add(element);
+    });
+  }
+
   @override
   void initState() {
     getCookBooks().then((value) => {setState(() {})});
-    getAllRecipes();
+    getAllPlantFoodFactoryRecipes();
     super.initState();
   }
 
@@ -152,11 +181,96 @@ class _HomeState extends State<Home> {
               ),
             ),
           ),
+          // Favorite Recipes
           Container(
               margin: const EdgeInsets.only(left: 20.0, right: 20.0),
               child: Column(
                 children: const [
-                  Text("Öffentliche Kochbücher:",
+                  Text("Lieblinge ❤️",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: fontSize,
+                          fontFamily: openSansFontFamily)),
+                  Divider(
+                    color: deepOrange,
+                    height: 20,
+                    thickness: 3,
+                  ),
+                ],
+              )),
+          SizedBox(
+            height: size.height * 0.2,
+            width: size.width,
+            child: ListView.builder(
+              shrinkWrap: true,
+              scrollDirection: Axis.horizontal,
+              itemCount: favoriteRecipes.length,
+              physics: const BouncingScrollPhysics(),
+              itemBuilder: (BuildContext context, int index) {
+                return GestureDetector(
+                    onTap: () => _openRecipeDetailPage(context, index),
+                    child: _buildFeaturedItem(
+                        image: favoriteRecipes.elementAt(index).image,
+                        title: '',
+                        subtitle: '',
+                        isFavoriteView: true));
+              },
+            ),
+          ),
+          SizedBox(height: size.height * 0.005),
+          // User Cookbooks
+          Container(
+              margin: const EdgeInsets.only(left: 20.0, right: 20.0),
+              child: Column(
+                children: const [
+                  Text("Kochbücher 🍽",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: fontSize,
+                          fontFamily: openSansFontFamily)),
+                  Divider(
+                    color: deepOrange,
+                    height: 20,
+                    thickness: 3,
+                  ),
+                ],
+              )),
+          FutureBuilder<List<Cookbook>>(
+            future: getUpdates(),
+            builder: (context, AsyncSnapshot<List<Cookbook>> snap) {
+              if (snap.data == null) {
+                return const Center(
+                    child: CircularProgressIndicator(
+                  color: basicColor,
+                ));
+              }
+              return ListView.builder(
+                  scrollDirection: Axis.vertical,
+                  shrinkWrap: true,
+                  itemCount: snap.data.length,
+                  physics: const BouncingScrollPhysics(),
+                  itemBuilder: (context, index) {
+                    return GestureDetector(
+                        onTap: () => _openDestinationPage(
+                            context,
+                            snap.data.elementAt(index).recipes,
+                            snap.data.elementAt(index),
+                            snap.data.length),
+                        child: _buildFeaturedItem(
+                          image: snap.data.elementAt(index).image,
+                          title: snap.data.elementAt(index).name,
+                          subtitle: "",
+                          isFavoriteView: false,
+                        ));
+                  });
+            },
+          ),
+          // Feed Me Cookbook
+          Container(
+              margin: const EdgeInsets.only(left: 20.0, right: 20.0),
+              child: Column(
+                children: const [
+                  Text("Feed Me's Kochbuch 🌿",
                       style: TextStyle(
                           color: Colors.white,
                           fontSize: fontSize,
@@ -182,52 +296,8 @@ class _HomeState extends State<Home> {
                   image:
                       "https://firebasestorage.googleapis.com/v0/b/feed-me-b8533.appspot.com/o/recipe_images%2FRed%20Curry%2F1.png?alt=media&token=bcfdf574-b959-45ff-a251-a171b2969161",
                   title: "Feed Me's Kochbuch",
-                  subtitle: 'Gesund & Lecker')),
-          Container(
-              margin: const EdgeInsets.only(left: 20.0, right: 20.0),
-              child: Column(
-                children: const [
-                  Text("Meine Kochbücher:",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: fontSize,
-                          fontFamily: openSansFontFamily)),
-                  Divider(
-                    color: deepOrange,
-                    height: 20,
-                    thickness: 3,
-                  ),
-                ],
-              )),
-          FutureBuilder<List<Cookbook>>(
-            future: getUpdates(),
-            builder: (context, AsyncSnapshot<List<Cookbook>> snap) {
-              if (snap.data == null) {
-                return const Center(
-                    child: CircularProgressIndicator(
-                  color: basicColor,
-                ));
-              }
-
-              return ListView.builder(
-                  scrollDirection: Axis.vertical,
-                  shrinkWrap: true,
-                  itemCount: snap.data.length,
-                  physics: const BouncingScrollPhysics(),
-                  itemBuilder: (context, index) {
-                    return GestureDetector(
-                        onTap: () => _openDestinationPage(
-                            context,
-                            snap.data.elementAt(index).recipes,
-                            snap.data.elementAt(index),
-                            snap.data.length),
-                        child: _buildFeaturedItem(
-                            image: snap.data.elementAt(index).image,
-                            title: snap.data.elementAt(index).name,
-                            subtitle: ""));
-                  });
-            },
-          ),
+                  subtitle: 'Gesund & Lecker',
+                  isFavoriteView: false)),
         ],
       ),
       floatingActionButton: FloatingActionButton(
@@ -249,7 +319,19 @@ class _HomeState extends State<Home> {
     );
   }
 
-  Container _buildFeaturedItem({String image, String title, String subtitle}) {
+  _openRecipeDetailPage(BuildContext context, int index) {
+    return Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (_) => DetailPage(
+                recipe: favoriteRecipes.elementAt(index),
+                reciptSteps: filterSteps(favoriteRecipes.elementAt(index)),
+                ingredients:
+                    filterIngredients(favoriteRecipes.elementAt(index)))));
+  }
+
+  Container _buildFeaturedItem(
+      {String image, String title, String subtitle, bool isFavoriteView}) {
     return Container(
       padding: const EdgeInsets.only(
           left: 16.0, top: 8.0, right: 16.0, bottom: 16.0),
@@ -270,27 +352,29 @@ class _HomeState extends State<Home> {
             ),
             Positioned(
               bottom: 20.0,
-              child: Container(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-                color: Colors.black.withOpacity(0.7),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: <Widget>[
-                    Text(title,
-                        style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 20.0,
-                            fontWeight: FontWeight.bold,
-                            fontFamily: openSansFontFamily)),
-                    Text(subtitle,
-                        style: const TextStyle(
-                            color: Colors.white,
-                            fontFamily: openSansFontFamily)),
-                  ],
-                ),
-              ),
+              child: !isFavoriteView
+                  ? Container(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 16.0, vertical: 8.0),
+                      color: Colors.black.withOpacity(0.7),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: <Widget>[
+                          Text(title,
+                              style: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.bold,
+                                  fontFamily: openSansFontFamily)),
+                          Text(subtitle,
+                              style: const TextStyle(
+                                  color: Colors.white,
+                                  fontFamily: openSansFontFamily)),
+                        ],
+                      ),
+                    )
+                  : Container(),
             ),
           ],
         ),
@@ -309,7 +393,7 @@ class _HomeState extends State<Home> {
         MaterialPageRoute(
             builder: (_) => RecipePage(
                   recipes: recipes,
-                  cookBookCount: cookBookCount-1,
+                  cookBookCount: cookBookCount - 1,
                   recipeCount: recipeCount,
                   cookBook: cookbook,
                   isFeedMeCookbook: isFeedMeCookbook,
