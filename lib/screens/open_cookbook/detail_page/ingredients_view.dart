74e1868ff69016746b50dcf61e5f48b6486e5813
Drescher, Maximilian <maximilian.drescher@student.fhws.de>
2022-01-21 14:13:22 +0100

    overhaul info row in detail page

--- lib/screens/open_cookbook/detail_page/ingredients_view.dart
+++ lib/screens/open_cookbook/detail_page/ingredients_view.dart
@@ -6,84 +6,25 @@ import '../../../constants/styles/text_style.dart';
 
 class IngredientsView extends StatelessWidget {
   final List<String> ingredients;
-  final String recipeTime;
-  final String recipeDifficulty;
-  final String personCount;
-  final String origin;
 
   const IngredientsView(
       {Key key,
-      @required this.ingredients,
-      @required this.origin,
-      @required this.personCount,
-      @required this.recipeTime,
-      @required this.recipeDifficulty})
+      @required this.ingredients,})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    double iconSize = 30.0;
-    double fontSizeForIcons = 14.0;
 
     Size size = MediaQuery.of(context).size;
     return SingleChildScrollView(
       child: Column(
         children: [
-          Row(
-            children: [
-              Icon(
-                Icons.person,
-                size: iconSize,
-                color: basicColor,
-              ),
-              Text(
-                personCount,
-                style: TextStyle(
-                  fontSize: fontSizeForIcons,
-                  color: Colors.black,
-                  fontFamily: openSansFontFamily,
-                ),
-              ),
-              Icon(
-                Icons.flag,
-                size: iconSize,
-                color: basicColor,
-              ),
-              Text(
-                origin,
-                style: TextStyle(
-                  fontSize: fontSizeForIcons,
-                  color: Colors.black,
-                  fontFamily: openSansFontFamily,
-                ),
-              ),
-              Icon(
-                Icons.timer,
-                size: iconSize,
-                color: basicColor,
-              ),
-              Text(
-                recipeTime + ' min',
-                style: TextStyle(
-                  fontSize: fontSizeForIcons,
-                  color: Colors.black,
-                  fontFamily: openSansFontFamily,
-                ),
-              ),
-              Icon(Icons.settings, size: iconSize, color: basicColor),
-              Text(
-                recipeDifficulty,
-                style: TextStyle(
-                  fontSize: fontSizeForIcons,
-                  color: Colors.black,
-                  fontFamily: openSansFontFamily,
-                ),
-              ),
-            ],
+          SizedBox(
+            height: size.height * 0.01,
           ),
           Container(
             padding: EdgeInsets.symmetric(
-                horizontal: size.width * 0.2, vertical: size.height * 0.035),
+                horizontal: size.width * 0.2, vertical: size.height * 0.025),
             child: ListView.builder(
                 shrinkWrap: true,
                 itemCount: ingredients.length,
