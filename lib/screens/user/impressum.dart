d4ad72322e507f00f503660b3060a9387c29e99d
Drescher, Maximilian <maximilian.drescher@student.fhws.de>
2022-03-14 11:14:14 +0100

    insert impressum

--- lib/screens/user/impressum.dart
+++ lib/screens/user/impressum.dart
@@ -1,4 +1,5 @@
 import 'package:animate_do/animate_do.dart';
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:feed_me/constants/styles/colors.dart';
 import 'package:feed_me/constants/styles/text_style.dart';
 import 'package:flutter/material.dart';
@@ -10,87 +11,53 @@ class Impressum extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Container(
-      height: size.height,
-      width: size.width,
-      decoration: const BoxDecoration(
-          gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [basicColor, deepOrange])),
-      child: Scaffold(
-          backgroundColor: Colors.transparent,
-          appBar: AppBar(
+        height: size.height,
+        width: size.width,
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [basicColor, deepOrange])),
+        child: Scaffold(
             backgroundColor: Colors.transparent,
-            elevation: 0,
-            title: const Text("Impressum",
-                style: TextStyle(
-                    color: Colors.black,
-                    fontFamily: openSansFontFamily,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20)),
-          ),
-          body: Center(
-            child: SizedBox(
-              width: size.width*0.8,
-              child: Column(
-                children: [
-                  FadeInDown(
-                      from: 100,
-                      duration: const Duration(milliseconds: 1000),
-                      child: Center(
-                        child: Container(
-                          height: size.height*0.2,
-                          width: size.width*0.8,
-                          decoration: const BoxDecoration(
-                              borderRadius: BorderRadius.all(Radius.circular(15.0)),
-                              image: DecorationImage(
-                                image: AssetImage(
-                                  "assets/CodeSeedLogo.png",
-                                ),
-                                fit: BoxFit.cover,
-                              )
-                          ),
-                        ),
-                      )
-                  ),
-
-                  SizedBox(height: size.height*0.02),
-
-                  FadeInUp(
-                    from: 100,
-                    duration: const Duration(milliseconds: 1000),
-                    child: const Text('Sämtliche Inhalte von FeedMe! sind urheberrechtlich geschützt. Die Vervielfältigung, Verbreitung und jede Art der Verwertung der Inhalte ist außerhalb der Grenzen des Urheberrechts untersagt. Als Inhalte werden im Kontext der Applikation neben den zur Verfügung gestellten Rezepten (inklusive des Bildmaterials), ebenfalls das Design, Logos und Grafiken verstanden.',
-                        style: TextStyle(
-                            fontFamily: openSansFontFamily,
-                            fontSize: 15)),
-                  ),
-                  SizedBox(height: size.height*0.02),
-                  FadeInUp(
-                    from: 100,
-                    duration: const Duration(milliseconds: 1000),
-                    child: const Text('Copyright © 2022 CodeSeed UG. Alle Rechte vorbehalten.',
-                        style:
-                        TextStyle(fontFamily: openSansFontFamily, fontSize: 16, fontWeight: FontWeight.bold)),
-                  ),
-                  FadeInUp(
-                    from: 100,
-                    duration: const Duration(milliseconds: 1000),
-                    child: const Text('codeseed.wue@gmail.com',
-                        style:
-                        TextStyle(fontFamily: openSansFontFamily, fontSize: 14)),
-                  ),
-                  FadeInUp(
-                    //TODO: Weiteres Rechtliches ergänzen
-                    from: 100,
-                    duration: const Duration(milliseconds: 1000),
-                    child: const Text('Hier noch rechtliche Sachen usw. ergänzen',
-                        style:
-                        TextStyle(fontFamily: openSansFontFamily, fontSize: 14)),
-                  )
-                ],
-              ),
+            appBar: AppBar(
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              title: const Text("Impressum",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontFamily: openSansFontFamily,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20)),
             ),
-          )),
-    );
+            body: SingleChildScrollView(
+              child: Center(
+                child: (Container(
+                  width: size.width * 0.95,
+                  height: size.height * 0.8,
+                  child: FadeInUp(
+                    from: 100,
+                    duration: const Duration(milliseconds: 1000),
+                    child: const AutoSizeText(
+                        'Angaben gem. § 5 TMG:\n'
+                        '\nCodeSeed UG.\n'
+                        'Tobias, Neidhardt und Maximilian, Drescher\nZum Rosenberg 2\n97334, Sommerach\n'
+                        '\nKontaktaufnahme:\n'
+                        'E-Mail: codeseed.wue@gmail.com\n'
+                        '\nUmsatzsteuer-ID\n'
+                        '\nUmsatzsteuer-Identifikationsnummer gem. § 27 a Umsatzsteuergesetz:\n'
+                        'DE XXX XXX XXX\n'
+                        '\nHaftungsausschluss – Disclaimer:\n'
+                        'Haftung für Inhalte Alle Inhalte unseres Internetauftritts wurden mit größter Sorgfalt und nach bestem Gewissen erstellt. Für die Richtigkeit, Vollständigkeit und Aktualität der Inhalte können wir jedoch keine Gewähr übernehmen. Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt.'
+                        'Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntniserlangung einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von den o.g. Rechtsverletzungen werden wir diese Inhalte unverzüglich entfernen.\n'
+                        '\nUrheberrecht:\n'
+                        'Die auf unserer Webseite veröffentlichen Inhalte und Werke unterliegen dem deutschen Urheberrecht (http://www.gesetze-im-internet.de/bundesrecht/urhg/gesamt.pdf) . Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung des geistigen Eigentums in ideeller und materieller Sicht des Urhebers außerhalb der Grenzen des Urheberrechtes bedürfen der vorherigen schriftlichen Zustimmung des jeweiligen Urhebers i.S.d. Urhebergesetzes (http://www.gesetze-im-internet.de/bundesrecht/urhg/gesamt.pdf ). Downloads und Kopien dieser Seite sind nur für den privaten und nicht kommerziellen Gebrauch erlaubt. Sind die Inhalte auf unserer Webseite nicht von uns erstellt wurden, sind die Urheberrechte Dritter zu beachten. Die Inhalte Dritter werden als solche kenntlich gemacht. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte unverzüglich entfernen.',
+                        stepGranularity: 1.0,
+                        style: TextStyle(
+                            fontFamily: openSansFontFamily, fontSize: 18)),
+                  ),
+                )),
+              ),
+            )));
   }
 }
